9a16f20f2b60e1b7db644bef5d0a2accc89e9a70
monejone <37658604+monejone@users.noreply.github.com>
2018-04-02 21:01:44 +0100

    added team members profiles as cards expanding into an overlay with more details

---
+++ js/overlay.js
@@ -0,0 +1,47 @@
+function on() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+}
+
+function on1() {
+    document.getElementById("overlay1").style.display = "block";
+}
+
+function off1() {
+    document.getElementById("overlay1").style.display = "none";
+}
+
+function on2() {
+    document.getElementById("overlay2").style.display = "block";
+}
+
+function off2() {
+    document.getElementById("overlay2").style.display = "none";
+}
+
+function on3() {
+    document.getElementById("overlay3").style.display = "block";
+}
+
+function off3() {
+    document.getElementById("overlay3").style.display = "none";
+}
+
+function on4() {
+    document.getElementById("overlay4").style.display = "block";
+}
+
+function off4() {
+    document.getElementById("overlay4").style.display = "none";
+}
+
+function on5() {
+    document.getElementById("overlay5").style.display = "block";
+}
+
+function off5() {
+    document.getElementById("overlay5").style.display = "none";
+}
